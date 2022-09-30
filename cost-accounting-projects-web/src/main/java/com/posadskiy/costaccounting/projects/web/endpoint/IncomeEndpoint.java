36c3fd5e6c56ca9b7de85ec63e872394aa64c117
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2022-09-30 22:38:24 +0200

    Fix URI

--- cost-accounting-projects-web/src/main/java/com/posadskiy/costaccounting/projects/web/endpoint/IncomeEndpoint.java
+++ cost-accounting-projects-web/src/main/java/com/posadskiy/costaccounting/projects/web/endpoint/IncomeEndpoint.java
@@ -3,26 +3,29 @@ package com.posadskiy.costaccounting.projects.web.endpoint;
 import com.posadskiy.costaccounting.moneyactions.api.request.IncomeRequest;
 import com.posadskiy.costaccounting.projects.core.controller.ProjectController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("income")
+@RequestMapping("project/income")
 public class IncomeEndpoint {
 
-	private final ProjectController projectController;
+    private final ProjectController projectController;
 
-	@Autowired
-	public IncomeEndpoint(ProjectController incomeController) {
-		this.projectController = incomeController;
-	}
+    @Autowired
+    public IncomeEndpoint(ProjectController incomeController) {
+        this.projectController = incomeController;
+    }
 
-	@PostMapping("add")
-	public void addIncome(@RequestBody final IncomeRequest incomeRequest) {
-		projectController.saveIncome(incomeRequest.getUserId(), incomeRequest.getIncome());
-	}
+    @PostMapping("add")
+    public void addIncome(@RequestBody final IncomeRequest incomeRequest) {
+        projectController.saveIncome(incomeRequest.getUserId(), incomeRequest.getIncome());
+    }
 
-	@PostMapping("delete")
-	public void deleteIncome(@RequestBody final IncomeRequest incomeRequest) {
-		projectController.deleteIncome(incomeRequest.getUserId(), incomeRequest.getIncome());
-	}
+    @PostMapping("delete")
+    public void deleteIncome(@RequestBody final IncomeRequest incomeRequest) {
+        projectController.deleteIncome(incomeRequest.getUserId(), incomeRequest.getIncome());
+    }
 }
--- cost-accounting-projects-web/src/main/java/com/posadskiy/costaccounting/projects/web/endpoint/PurchaseEndpoint.java
+++ cost-accounting-projects-web/src/main/java/com/posadskiy/costaccounting/projects/web/endpoint/PurchaseEndpoint.java
@@ -2,27 +2,28 @@ package com.posadskiy.costaccounting.projects.web.endpoint;
 
 import com.posadskiy.costaccounting.moneyactions.api.request.PurchaseRequest;
 import com.posadskiy.costaccounting.projects.core.controller.ProjectController;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("purchase")
+@RequestMapping("project/purchase")
 public class PurchaseEndpoint {
 
-	private final ProjectController purchaseController;
+    private final ProjectController purchaseController;
 
-	public PurchaseEndpoint(ProjectController purchaseController) {
-		this.purchaseController = purchaseController;
-	}
+    public PurchaseEndpoint(ProjectController purchaseController) {
+        this.purchaseController = purchaseController;
+    }
 
-	@PostMapping("add")
-	public void addPurchase(@RequestBody final PurchaseRequest purchaseRequest) {
-		purchaseController.savePurchase(purchaseRequest.getUserId(), purchaseRequest.getPurchase());
-	}
-	
-	@PostMapping("delete")
-	public void deletePurchase(@RequestBody final PurchaseRequest purchaseRequest) {
-		purchaseController.deletePurchase(purchaseRequest.getUserId(), purchaseRequest.getPurchase());
-	}
+    @PostMapping("add")
+    public void addPurchase(@RequestBody final PurchaseRequest purchaseRequest) {
+        purchaseController.savePurchase(purchaseRequest.getUserId(), purchaseRequest.getPurchase());
+    }
+
+    @PostMapping("delete")
+    public void deletePurchase(@RequestBody final PurchaseRequest purchaseRequest) {
+        purchaseController.deletePurchase(purchaseRequest.getUserId(), purchaseRequest.getPurchase());
+    }
 }
